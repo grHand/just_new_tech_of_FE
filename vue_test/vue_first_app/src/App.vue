af96ebbc523404fbff5f3ea9bc13c8f3a71a2e8f
wangxiaolin <wangxiaolin1993@gmail.com>
2016-08-08 21:33:29 +0800

    just demo

---
+++ vue_test/vue_first_app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    <world></world>
+  </div>
+</template>
+<script>
+import Hello from './components/Hello.vue'
+import World from './components/world.vue'
+
+export default {
+  components: {
+    Hello,
+    World
+  },
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ vue_test/vue_first_app/src/components/world.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="world">
+        <p>{{ inputdata }}</p>
+        <input v-model="inputdata">
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                inputdata: 'world'
+            }
+        }
+    }
+</script>
